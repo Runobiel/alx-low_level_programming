aa15f61523d7d35a914d28affbe56d4bc0873116
Runobiel <egberuno@gmail.com>
2022-09-08 17:49:11 -0700

    Write words in C using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- entry point for the program
+ *
+ * Return: returns 0 if no error, else return non-zero value
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
