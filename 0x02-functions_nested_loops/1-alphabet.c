835bb8c9e7afba8590b70993dcb12cba6f672a40
Runobiel <egberuno@gmail.com>
2022-09-13 22:03:31 -0700

    Print lower case in new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Entry point
+  *
+  * Description: prints the alphabet, in lowercase
+  * Return: Always 0 (Success)
+  */
+
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
