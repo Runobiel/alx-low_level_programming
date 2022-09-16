76f46fc27c91a887cdfdfb81c639c8e80145f0db
Runobiel <egberuno@gmail.com>
2022-09-15 17:47:35 -0700

    Test positve_or_negative function

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main.h
+#define main.h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_numbers(int a, int b, int c);
+void print_remaining_days(int month, int day, iint year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
