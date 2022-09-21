8a24f0c398a59a35f183c1899ea796f7de2b115b
Runobiel <egberuno@gmail.com>
2022-09-20 19:49:31 -0700

    Correction to -A function that prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _puts: This is the entry point
- * @str: is string to be printed
+ * _puts- is entry point
+ * @str: string to be printed
  *
- * Description: prints a string, followed by a new line to stdout
- * 
- * Returns: void
+ * Description- prints a string, followed by a new line to stdout
+ *
+ * Returns:string
  */
 
 void _puts(char *str)
 {
 	int i;
 
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
