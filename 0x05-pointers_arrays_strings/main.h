c0905cf2b88bfd8236b773ac0e3ec03e010bd83a
Runobiel <egberuno@gmail.com>
2022-09-20 23:58:48 -0700

    main header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
-#ifdef main_h
+#ifndef main_h
 #define main_h
 
 #include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void sawp_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
