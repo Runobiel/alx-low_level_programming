2150b8f793aa9e0af941a80b023aee11ce0e0963
Runobiel <egberuno@gmail.com>
2022-09-20 20:22:38 -0700

    Print a string and new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts- is entry point
+ * _puts- entry point
  * @str: string to be printed
  *
  * Description- prints a string, followed by a new line to stdout
  *
- * Returns:string
+ * Return: no return value
  */
 
 void _puts(char *str)
