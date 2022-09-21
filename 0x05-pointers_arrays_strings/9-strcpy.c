5f10e7b483b525165adf958e14777c754bd44047
Runobiel <egberuno@gmail.com>
2022-09-21 00:21:19 -0700

    print a string and a new line

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,59 +1,30 @@
-#include "main.h"                                                                                                                                 /**                                                                                                                                                                   
+#include "main.h"
+
+/**
+ * _strcpy - entry point
  *
- *  * _strcpy - entry point                                                                                                                                                  
+ * Description: copies the string pointed to by src, including (\0)
  *
- *   *                                                                                                                                                                        
+ * @src: source string
+ * @dest: destination string
  *
- *    * Description: copies the string pointed to by src, including (\0)                                                                                                       
- *
- *     *                                                                                                                                                                        
- *
- *      * @src: source string                                                                                                                                                    
- *
- *       * @dest: destination string                                                                                                                                              
- *
- *        *                                                                                                                                                                        
- *
- *         * Return: pointer to destination                                                                                                                                         
- *
- *          */                                                                                                                                                                       
+ * Return: pointer to destination
+ */
 
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
 
+	len = 0;
 
-char *_strcpy(char *dest, char *src)                                                                                                                                      
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i <= len ; i++)
+	{
+		dest[i] = src[i];
+	}
 
-{                                                                                                                                                                         
-
-	        int i, len;                                                                                                                                                       
-
-
-
-		len = 0;                                                                                                                                                          
-
-
-
-		        while (src[len] != '\0')                                                                                                                                          
-
-				        {                                                                                                                                                                 
-
-						len++;                                                                                                                                                    
-
-						        }                                                                                                                                                                 
-
-			        for (i = 0; i<= len ;i++)                                                                                                                                       
-
-					        {                                                                                                                                                                 
-
-							dest[i] = src[i];                                                                                                                                         
-
-							        }                                                                                                                                                                 
-
-
-
-				        return (dest);                                                                                                                                                    
-
-
-
-}  
-
-
+	return (dest);
+}
