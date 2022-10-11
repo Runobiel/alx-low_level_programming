36ed913c2d2690903f4f37b20b0acfeecd62e605
Runobiel <egberuno@gmail.com>
2022-10-10 20:02:06 -0700

    Define a new type struct dog with elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,5 @@
-#include <stdio.h>
-
-/**
- * struct dog - a new type of structure
- * @name - sets the dog name
- * @age - sets the dog age
- * @owner - sets the dog owner
- *
- * Description - a structure that declares the name,age and owner of struct dog
- *
- */
+#ifndef dog_h
+#define dog_h
 
 struct dog
 {
@@ -16,21 +7,7 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * main- is the entry point
- * @dog1- is the struct varriable
- * dog1.name- accesses the struct member char *name
- * dog1.float- accesses the struct member float *age
- * dog1.owner- accesses the struct member char *owner
- */
-
-int main ();
-{
-	struct dog dog1;
-	strcpy(dog1.name, "shirely");
-	dog1.age = 2.13;
-	strcpy(dog1.owner, "Grace");
-	ptintf("dog name is: %s\ndog age is: %f\ndog owner is: %s\n", dog1.name, dog1.age, dog1.owner);
-	return 0;
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
