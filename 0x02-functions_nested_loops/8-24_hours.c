bdbc17437a7ae78da90a638d7a950c074ad6b44b
Runobiel <egberuno@gmail.com>
2022-10-25 06:19:25 -0700

    A function that prints the last digit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - calls function
+ * jack_bauer - entry point
  * Description: A function that prints every minute of
  * the day of Jack Bauer, starting from 00:00 to 23:59.
  * Return: 0
